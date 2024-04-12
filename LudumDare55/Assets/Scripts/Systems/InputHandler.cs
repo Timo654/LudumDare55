da18caa65243d463d2eccf9cad44e0df10e5f8bb
Timo654 <41972888+Timo654@users.noreply.github.com>
2024-04-12 23:20:10 +0300

    add menus and some scenes and stuffs

---
+++ LudumDare55/Assets/Scripts/Systems/InputHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    // This class is for handling input stuff. It could be better but hopefully it'll work well enough
+    // and make it easier to disable buttons during certain situations...
+
+    // INPUTS
+    public static Action pause;
+    public static Action back;
+
+    // BOOLS TO CHECK STUFF
+    private bool isPaused = false;
+
+    private InputAction pauseAction;
+    private InputAction backAction;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerControls playerControls = new();
+        pauseAction = playerControls.UI.Pause;
+        backAction = playerControls.UI.Back;
+    }
+
+    private void OnEnable()
+    {
+        pauseAction.Enable();
+        backAction.Enable();
+        pauseAction.performed += PausePerformed;
+        backAction.performed += BackPerformed;
+        PauseMenuController.GamePaused += TogglePause;
+    }
+
+    private void OnDisable()
+    {
+        pauseAction.Disable();
+        backAction.Disable();
+        pauseAction.performed -= PausePerformed;
+        backAction.performed -= BackPerformed;
+        PauseMenuController.GamePaused -= TogglePause;
+    }
+
+    private void TogglePause(bool obj)
+    {
+        isPaused = obj;
+    }
+
+    private void PausePerformed(InputAction.CallbackContext context)
+    {
+        pause?.Invoke();
+    }
+
+    private void BackPerformed(InputAction.CallbackContext context)
+    {
+        back?.Invoke();
+    }
+}
---
+++ LudumDare55/Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,163 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static Action<bool> GamePaused;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject pauseButtons;
+    [SerializeField] private Button settingsBackButton;
+    [SerializeField] private GameObject firstSelectedUIElement;
+    [SerializeField] private GameObject touchUI;
+    private PlayerControls playerControls;
+    private InputAction pauseAction;
+    private InputAction backAction;
+    private CanvasGroup optionsMenuCG;
+    private CanvasGroup pauseMenuCG;
+    private CanvasGroup pauseButtonsCG;
+    private EventSystem EVRef;
+    private GameObject lastSelect;
+    private bool canPause;
+    private bool isPaused;
+    private bool pauseSubscribed = true;
+
+    private void Awake()
+    {
+        UIFader.InitializeFader();
+        EVRef = EventSystem.current;
+        optionsMenuCG = optionsMenu.AddComponent<CanvasGroup>();
+        pauseMenuCG = pauseMenu.AddComponent<CanvasGroup>();
+        pauseButtonsCG = pauseButtons.AddComponent<CanvasGroup>();
+        playerControls = new PlayerControls();
+        pauseAction = playerControls.UI.Pause;
+        backAction = playerControls.UI.Back;
+        lastSelect = firstSelectedUIElement;
+        if (touchUI != null)
+        {
+            if (BuildConstants.isMobile)
+            {
+                touchUI.SetActive(true);
+            }
+            else
+            {
+                touchUI.SetActive(false);
+            }
+        }
+    }
+
+    public void OnEnable()
+    {
+        pauseAction.Enable();
+        InputHandler.pause += OnPauseButton;
+        LevelChanger.OnFadeInFinished += HandleFadeIn;
+    }
+
+    public void OnDisable()
+    {
+        InputHandler.pause -= OnPauseButton;
+        InputHandler.back -= OnPauseButton;
+        LevelChanger.OnFadeInFinished -= HandleFadeIn;
+    }
+
+    void HandleFadeIn()
+    {
+        canPause = true;
+    }
+
+    private void Update()
+    {
+        if (isPaused)
+        {
+            if (EVRef.currentSelectedGameObject == null)
+            {
+                EVRef.SetSelectedGameObject(lastSelect);
+            }
+            else
+            {
+                lastSelect = EVRef.currentSelectedGameObject;
+            }
+        }
+        else if (!pauseSubscribed)
+        {
+            SubscribeToPause();
+        }
+    }
+
+    public void OnPauseButton()
+    {
+        if (settingsBackButton != null && settingsBackButton.IsActive()) settingsBackButton.onClick.Invoke();
+        else HandlePause();
+    }
+
+    public void HandlePause()
+    {
+        if (!canPause) return;
+        if (optionsMenu != null && optionsMenu.activeSelf)
+        {
+            HandleOptions();
+            return;
+        }
+        if (isPaused) UnpauseGame();
+        else PauseGame();
+    }
+
+    void PauseGame()
+    {
+        if (Time.timeScale < 1.0f) return;
+        GamePaused?.Invoke(true);
+        Time.timeScale = 0f;
+        UIFader.FadeCanvasGroup(pauseMenuCG);
+        EVRef.SetSelectedGameObject(firstSelectedUIElement);
+        AudioManager.Pause();
+        backAction.Enable();
+        isPaused = true;
+    }
+
+    void UnpauseGame()
+    {
+        SubscribeToPause();
+        Time.timeScale = 1f;
+        GamePaused?.Invoke(false);
+        pauseMenu.SetActive(false);
+        AudioManager.Unpause();
+        backAction.Disable();
+        isPaused = false;
+    }
+
+    private void SubscribeToBack()
+    {
+        if (!pauseSubscribed) return;
+        InputHandler.back += OnPauseButton;
+        InputHandler.pause -= OnPauseButton;
+        pauseSubscribed = false;
+    }
+
+    private void SubscribeToPause()
+    {
+        if (pauseSubscribed) return;
+        InputHandler.back -= OnPauseButton;
+        InputHandler.pause += OnPauseButton;
+        pauseSubscribed = true;
+    }
+
+    public void HandleOptions()
+    {
+        if (optionsMenu.activeSelf)
+        {
+            UIFader.FadeObjects(pauseButtons, pauseButtonsCG, optionsMenu, optionsMenuCG);
+        }
+        else
+        {
+            UIFader.FadeObjects(optionsMenu, optionsMenuCG, pauseButtons, pauseButtonsCG);
+        }
+    }
+
+    public void HandleQuit()
+    {
+        LevelChanger.Instance.FadeToLevel("MainMenu");
+    }
+}
